c7bb3814a990e9ac415ecf8564fc4baad9b6a089
ido <idoitzhak5@gmail.com>
2022-03-14 13:11:50 +0200

    1403

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-//names
+//שמות המשתנים
 
 const express = require('express');
 const bodyparser = require('body-parser');
 const app = express();
 
+//קריאה 
 app.use(bodyparser.urlencoded());
 app.use(bodyparser.json());
 
-app.listen(5000);
+const actionsRoute = require('./actions');
+app.use('/api',actionsRoute);
+
+//השרת מקשיב
+const port = 5000;
+app.listen(port,function(){
+    console.log(`Server is working via port ${port}`);
+});
